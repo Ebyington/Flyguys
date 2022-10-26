85e4aebff4cd1053df3b4ce5d972fe5be1a04a94
Novo Persona <adcaldwell93@yahoo.com>
2022-10-26 14:12:41 -0400

    added models for posts in posts.js under models folder

---
+++ models/posts.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Posts extends Model {}
+
+Posts.init(
+
+    {
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },  
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        typeOfDrone: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        tags: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'posts'
+    }
+);
+
+module.exports = Posts;
