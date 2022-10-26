bffb0e9bc885ea4730a5cd32a383154b2772afeb
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-10-26 14:06:49 -0400

    events

---
+++ models/Meetups.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes}= require('sequelize');
+const sequelize = require('../config/config');
+
+class Meetups extends Model{}
+
+Meetups.init(
+    {
+        title: DataTypes.STRING,
+        description: DataTypes.STRING,
+        location: DataTypes.STRING,
+        image: DataTypes.STRING,
+        counter: DataTypes.INTEGER
+        
+    },
+    {
+        sequelize,
+        timestamps: true,
+        modelName: 'Meetups'
+    }
+)
+
+module.exports= Meetups;
