22dd44ea79bbc67f59b4a1a41085a7373d14e7e0
Eric Byington <112353490+Ebyington@users.noreply.github.com>
2022-10-26 20:35:36 -0400

    Delete routes/api directory

--- routes/api/userRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const User = require('../../models/User');
-const userRoutes = require('../../models/User');
-
-router.get('/', async (req, res) => {
-    const userData = await User.
-});
-
-module.exports = router;
