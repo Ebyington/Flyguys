37188b0991e5f01283facaffdaf6bd216b8a29fe
Eric <ericbyington33@gmail.com>
2022-10-27 13:26:26 -0400

    dash routes.js, added edit, delete and meetups

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -1,5 +1,5 @@
 const router= require('express').Router();
-const { Post } = require('../models/posts');
+const { Post, Meetups } = require('../models');
 
 // const withAuth = require('../utils/auth');
 // needs withAuth before deploy, left out for db testing
@@ -7,15 +7,34 @@ const { Post } = require('../models/posts');
     try {
         const postData = await Post.finAll({
             where: {
-                userId: req.userId,
+                user_id: req.user_id,
             },
         });
         const posts = postData.map((post) => post.get({ plain: true}));
 
         res.render('postHistory', {
             layout: 'dashboard',
-            post,
+            posts,
         });
+        
+    } catch (err) {
+        res.redirect('login');
+    }
+ });
+ router.get('/', async (req, res) => {
+    try {
+        const meetData = await Meetups.finAll({
+            where: {
+                user_id: req.user_id,
+            },
+        });
+        const meetups = meetData.map((Meetups) => meetups.get({ plain: true}));
+
+        res.render('postHistory', {
+            layout: 'dashboard',
+            Meetups,
+        });
+        
     } catch (err) {
         res.redirect('login');
     }
@@ -27,5 +46,41 @@ const { Post } = require('../models/posts');
     });
  });
 
+ router.delete('/:id', async (req, res) => {
+    try {
+      const [aRows] = Post.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (aRows > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+ router.get('/edit/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id);
+  
+      if (postData) {
+        const post = postData.get({ plain: true });
+  
+        res.render('edit', {
+          layout: 'dashboard',
+          post,
+        });
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.redirect('login');
+    }
+  });
 
 module.exports= router;
