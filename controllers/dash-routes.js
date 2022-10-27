12dc7d38035c05eb588b3fc202fa65d6fe8ff73b
Eric <ericbyington33@gmail.com>
2022-10-26 23:53:55 -0400

    began to build get routes for user dashboard

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -1,2 +1,31 @@
 const router= require('express').Router();
+const { Post } = require('../models/posts');
+
+// const withAuth = require('../utils/auth');
+// needs withAuth before deploy, left out for db testing
+ router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.finAll({
+            where: {
+                userId: req.userId,
+            },
+        });
+        const posts = postData.map((post) => post.get({ plain: true}));
+
+        res.render('postHistory', {
+            layout: 'dashboard',
+            post,
+        });
+    } catch (err) {
+        res.redirect('login');
+    }
+ });
+// new post get for user profile dash
+ router.get('/new', (req, res) => {
+    res.render('newpost', {
+        layout: 'dashboard',
+    });
+ });
+
+
 module.exports= router;
