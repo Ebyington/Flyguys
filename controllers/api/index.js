97d2796b443f82ea19d3e978e7feefab1abfdd8f
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-10-27 17:36:16 -0400

    created profile routes and finished post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,9 +5,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-route');
 const postRoutes = require('./postsRoutes');
 const meetRoutes = require('./meetups-routes');
+const ProfileRoutes = require('./profile-routes');
 
 router.use('/users', userRoutes);
 router.use('/meetups', meetRoutes);
-router.use('/posts', postRoutes)
+router.use('/posts', postRoutes);
+router.use('/profile', ProfileRoutes);
 
 module.exports = router;
