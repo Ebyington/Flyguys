f78c48af7f59cb26016b822910afb34eea6daac4
Novo Persona <adcaldwell93@yahoo.com>
2022-10-26 20:35:19 -0400

    created get & post routes.need to get routes to work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,11 @@
 
 const router = require('express').Router();
 
-const userRoutes = require('/user-route.js');
+const userRoutes = require('./user-route');
+
+const postRoutes = require('./postsRoutes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
---
+++ controllers/api/postsRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { User, Posts} = require('../../models/Posts');
+
+router.get('/', async (req, res) => {
+    try {
+        const userPosts = await Posts.findAll({
+            include: [{model:User}],
+        });
+        res.status(200).json(userPosts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const userPosts = await Posts.create({
+          description: req.body.description,
+          typeOfDrone: req.body.typeOfDrone,
+          tags: req.body.tags,  
+        });
+        res.status(200).json(userPosts);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+module.exports = router;
