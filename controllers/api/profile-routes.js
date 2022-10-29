0d0f8c1e30736045f16fc69df4259946b3cc3c1e
Eric <ericbyington33@gmail.com>
2022-10-29 18:13:32 -0400

    heroku sucks

--- controllers/api/profile-routes.js
+++ controllers/api/profile-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Profile, User} = require('../../models/');
+const { Profile, User} = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
