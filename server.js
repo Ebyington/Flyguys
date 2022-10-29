498ef32659882bf3916dc2d94d253eab877006a4
Eric <ericbyington33@gmail.com>
2022-10-29 13:34:54 -0400

    fixed youtube

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(require('./controllers'));
+
 /////add once routes are up
 
 app.listen(PORT, () => {
