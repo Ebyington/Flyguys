06378545384ad09bb903f0f2c3e732e95cf8e48d
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-10-31 00:44:48 -0400

    some more needs done but almost there

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -3,14 +3,18 @@ const postId = document.querySelector('input[name="post-id"]').value;
 const editForm = async function(event) {
   event.preventDefault();
 
-  const  typeOfDrone= document.querySelector('input[name="typeOfDrone"]').value;
+  const typeOfDrone= document.querySelector('input[name="typeOfDrone"]').value;
   const tags = document.querySelector('input[name="tags"]').value;
+  const  image= document.querySelector('input[name="image"]').value;
+  const videoLink = document.querySelector('input[name="videoLink"]').value;
   const description= document.querySelector('textarea[name="description"]').value;
 
   await fetch(`/api/Posts/${postId}`, {
     method: 'PUT',
     body: JSON.stringify({
         typeOfDrone,
+        image,
+        videoLink,
         description,
         tags,
     }),
